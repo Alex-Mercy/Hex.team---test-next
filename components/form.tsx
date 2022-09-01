a402877c4a924583da45a633904305c7317f9577
Alex <hambiru@yandex.ru>
2022-09-01 21:16:05 +0300

    react hook form and validation

--- components/form.tsx
+++ components/form.tsx
@@ -1,14 +1,46 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 import styles from '../styles/Form.module.scss'
 
+type FormValue = {
+  link: string
+}
+
 const Form = () => {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm<FormValue>({
+    mode: 'onBlur',
+  })
+
+  const onSubmit = (data: FormValue) => {
+    console.log(data)
+    reset()
+  }
+
   return (
-    <div className={styles.form}>
-      <input type='text' placeholder='Shorten a link here...' className={styles.input} />
-      <a href='' className={styles.button}>
-        Shorten
-      </a>
+    <div>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type='text'
+          {...register('link', {
+            required: 'Please, provide a valid url',
+            pattern: {
+              value: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/g,
+              message: 'Please, provide a valid url',
+            },
+          })}
+          placeholder='Shorten a link here...'
+          className={styles.input}
+        />
+        <button type='submit' className={styles.button}>
+          {errors?.link ? <div className={styles.error}>{errors.link.message} </div> : 'Shorten'}
+        </button>
+      </form>
     </div>
   )
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,18 +1,53 @@
-import React from 'react'
+import React, { FC } from 'react'
 let cn = require('classnames')
+import { useForm } from 'react-hook-form'
 
 import styles from '../styles/Login.module.scss'
 
-const SignUp = () => {
+type FormValues = {
+  email: string
+  password: string
+}
+
+const SignUp: FC = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormValues>({
+    mode: 'onBlur',
+  })
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data)
+  }
+
   return (
     <div className={styles.login}>
       <h1 className={styles.header}>Sign Up</h1>
-      <form className={styles.form}>
-        <input type='text' placeholder='Email Address' className={cn(styles.input, styles.email)} />
-        <input type='text' placeholder='Password' className={cn(styles.input, styles.password)} />
-        <a href='' className={styles.button}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+        <input
+          {...register('email', {
+            required: 'Email is required.',
+            pattern: { value: /\S+@\S+\.\S+/, message: 'Invalid email format' },
+          })}
+          placeholder='Email Address'
+          className={cn(styles.input, styles.email)}
+        />
+        {errors?.email && <div className={styles.errorMessage}>{errors.email.message}</div>}
+        <input
+          {...register('password', {
+            required: 'Password is required.',
+            minLength: { value: 5, message: '5 characters minimum' },
+          })}
+          type='password'
+          placeholder='Password'
+          className={cn(styles.input, styles.password)}
+        />
+        {errors?.password && <div className={styles.errorMessage}>{errors.password.message}</div>}
+        <button type='submit' className={styles.button}>
           Sign Up
-        </a>
+        </button>
       </form>
     </div>
   )
